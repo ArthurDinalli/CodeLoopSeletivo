edd227ca732ac69059cb4ec96b3926356141d1ac
ArthurDinalli <arthurdabreu@hotmail.com>
2019-05-11 22:53:38 -0300

    Adicionado blacklist na validacao do cpf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -260,7 +260,7 @@ export class AppComponent implements OnInit {
     var Soma;
     var Resto;
     Soma = 0;
-    if (cpf == "00000000000") return false;
+    if (cpf == "00000000000" || cpf == "11111111111" || cpf == "22222222222" || cpf == "33333333333" || cpf == "44444444444" || cpf == "55555555555" || cpf == "66666666666" || cpf == "77777777777" || cpf == "88888888888" || cpf == "99999999999" ) return false;
      
     for (var i=1; i<=9; i++) Soma = Soma + parseInt(cpf.substring(i-1, i)) * (11 - i);
       Resto = (Soma * 10) % 11;
@@ -271,13 +271,14 @@ export class AppComponent implements OnInit {
     Soma = 0;
     for (i = 1; i <= 10; i++) 
       Soma = Soma + parseInt(cpf.substring(i-1, i)) * (12 - i);
-    Resto = (Soma * 10) % 11;
+      Resto = (Soma * 10) % 11;
    
     if ((Resto == 10) || (Resto == 11))  Resto = 0;
     if (Resto != parseInt(cpf.substring(10, 11) ) ) return false;
     return true;
 
-}
+  }
+  
   private validacao() {
     if(this.alunoForm.nome == ''){
       this.alunoValidation.nome = true;
